4225c531126ffbfb618537111fb25c1d27ad6574
testpurpose1667 <testpurpose1667@gmail.com>
2022-03-01 18:08:28 +0530

    hour 2 completed

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,7 +1,8 @@
+
 pragma solidity ^0.6.0;
 
 contract SimpleStorage {
-    uint256 favoriteNumber = 5;
+    uint256 public favoriteNumber = 5;
     bool favoriteBool = true;
     string favString = "string";
     int256 favInt = 5;
@@ -9,5 +10,33 @@ contract SimpleStorage {
     bytes32 favBytes ="cat";
     bytes2 twobytes = "rt";
     
+    struct People {
+        uint256 favoriteNumber  ;
+        string name;
+    }
+    // create a array ;
+    People[] public people;
 
+    // memory ?? what is storage after execution delete _name
+    function addPerson(string memory _name , uint256 _favNum) public  {
+        people.push(People({favoriteNumber : _favNum  , name : _name  }));
+    }
+    People public person1 = People({
+        favoriteNumber : 999, 
+        name : "saptadeep"
+    });
+
+
+    function store(uint256 _favNum) public {
+        favoriteNumber = _favNum;
+
+    }
+    // we are just viewing
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+ 
+   }
+    function add(uint x) public pure {
+        x + x;
+    }
 }
