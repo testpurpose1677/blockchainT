94fab9e4b044e2fd427b76dfed8a3653b1746216
testpurpose1667 <testpurpose1667@gmail.com>
2022-03-01 15:54:43 +0530

    first solidity code simple contract written

---
+++ SimpleStorage.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+    uint256 favoriteNumber = 5;
+    bool favoriteBool = true;
+    string favString = "string";
+    int256 favInt = 5;
+    address favAdd = 0xC418bc2Ff7e40Ad36f4BD36ccfF2d0673A3F93BB;
+    bytes32 favBytes ="cat";
+    bytes2 twobytes = "rt";
+    
+
+}
